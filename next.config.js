c4926a6de94c6aa182757afb6570d0bb38facc51
mr-d0nuT <94043977+mr-d0nuT@users.noreply.github.com>
2026-03-15 17:30:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Esto es vital para que GitHub Pages encuentre tus archivos
+  basePath: '/TimeoutBCN', 
   images: {
     unoptimized: true,
   },
